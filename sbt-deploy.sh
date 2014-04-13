ea6bc31fa56a654bc07a8f971b8daab99b4d36a5
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-04-14 08:51:46 +1000

    Fixed deploy script.

--- sbt-deploy.sh
+++ sbt-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -euv
 
-test $TRAVIS_PULL_REQUEST == "false" \
-    && test $TRAVIS_BRANCH == "master" && \
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
     sbt -Dsbt.global.base=$TRAVIS_BUILD_DIR/ci ';set publishTo in ThisBuild := Some("commbank-releases" at "http://commbank.artifactoryonline.com/commbank/ext-releases-local"); set publishMavenStyle in ThisBuild  := true; publish'
+else
+    echo "Not on master. Nothing to deploy"
+fi
