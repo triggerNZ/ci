974eceeef0263b819a4126f53df4ba6c8d15b094
Leo Venegas <leo.venegas@cba.com.au>
2016-05-02 17:39:47 +1000

    Updated the script gh-commit-comment.sh to use getopts

--- gh-commit-comment.sh
+++ gh-commit-comment.sh
@@ -8,16 +8,10 @@ set -e
 # our specific use case is to have our CI system post comments against a commit
 # which is part of a pull request.
 #
-# the script expects the following parameters:
-#   $1 - GH or GHE endpoint E.g. https://api.github.com
-#   $2 - GitHub API user
-#   $3 - GitHub API user personal token
-#   $4 - GitHub repository owner and name E.g. lvenegas/myrepo
-#   $5 - Git commit-sha to comment against
-#   $6 - Comment
-#
 # example call:
-#   ./gh-commit-comment.sh https://api.github.com lvenegas abcde lvenegas/myrepo ba7ead 'Comment here!'
+#   ./gh-commit-comment.sh -e http://code.br.zbi.cba/api/v3 -u zbi-ci -p xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -r ZBI/zbi-infra-dev -s ad9e0fadf1658ecf0fac0068c4e923ee26e73bc3 -f gh-comment.json
+#
+# please see the comments below for details on each command line flag
 #
 # NOTE: on failure it returns a non-zero exit code so that our CI process
 # registers the error
@@ -25,9 +19,52 @@ set -e
 # see the page below for the API details:
 # - https://developer.github.com/v3/repos/comments/#create-a-commit-comment
 
+usage() { echo "Usage: $0 [-e <GH or GHE endpoint>] [-u <API user>] [-p <API personal token>] [-r <repository>] [-s <commit-sha>] [-f <JSON comments file>]" 1>&2; }
+
+while getopts ":e:u:p:r:s:f:" o; do
+    case "${o}" in
+        e)
+            # GitHub or GHE endpoint E.g. https://api.github.com
+            e=${OPTARG}
+            ;;
+        u)
+            # GitHub or GHE API user
+            u=${OPTARG}
+            ;;
+        p)
+            # GitHub or GHE API user personal token
+            p=${OPTARG}
+            ;;
+        r)
+            # repository is in the form of username/repo E.g. CommBank/zbi
+            r=${OPTARG}
+            ;;
+        s)
+            # Git commit-sha to comment against
+            s=${OPTARG}
+            ;;
+        f)
+            # file with JSON payload to submit
+            # e.g. the file gh-comment.json with content as follows
+            # {"body":"Your comment here"}
+            f=${OPTARG}
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${e}" ] || [ -z "${u}" ] || [ -z "${p}" ] || [ -z "${r}" ] || [ -z "${s}" ] || [ -z "${f}" ]; then
+    usage
+    exit 1
+fi
+
 curl \
   --fail \
-  -u $2:$3 \
+  -u $u:$p \
   -H "Content-Type: application/json" \
-  -X POST -d "{\"body\": \"$6\"}" \
-  $1/repos/$4/commits/$5/comments
+  -X POST --data @$f \
+  $e/repos/$r/commits/$s/comments
